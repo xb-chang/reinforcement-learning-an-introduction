248fe674f662d6865ece59eba9397c0a20d3963c
xb-chang <x_chang@DESKTOP-CA0DO21.localdomain>
2019-08-17 15:44:20 +0100

    Init set.

---
+++ chapter03/my_grid_world.py
@@ -0,0 +1,13 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.table import Table
+
+matplotlib.use('Agg')
+
+WORLD_SIZE = 5
+A_POS = [0, 1]
+A_PRIME_POS = [4, 1]
+B_POS = [0, 3]
+B_PRIME_POS = [2, 3]
+DISCOUNT = 0.9
