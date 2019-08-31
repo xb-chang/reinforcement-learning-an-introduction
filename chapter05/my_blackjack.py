7900bda937a9106a0cb5c8dcf764918887ef1b45
xb-chang <x_chang@DESKTOP-CA0DO21.localdomain>
2019-08-31 22:11:30 +0100

    fig_5_1 done

--- chapter05/my_blackjack.py
+++ chapter05/my_blackjack.py
@@ -1,4 +1,9 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from random import randint
 
 import pdb
@@ -60,6 +65,7 @@ def bj_eposide_gen(p_policy, d_policy):
     dealer_show = dealer_cards[0]
     ace_heat, p_sum = sum_cards(player_cards)
 
+    # state and action belong to t
     state_list = []
     state_list.append((p_sum, dealer_show, ace_heat))
 
@@ -67,6 +73,7 @@ def bj_eposide_gen(p_policy, d_policy):
     action = p_policy[p_sum]
     action_list.append(action)
     
+    # reward belong to t+1
     reward_list = [] # R_{t+1}
 
     
@@ -136,23 +143,99 @@ def figure_5_1():
 
     # state vaiues
     # usable ace state
-    U_ACE_V = np.zeros((10, 10))
+    U_ACE_V = np.zeros((10, 10), dtype=np.float)
+    U_ACE_C = np.zeros((10, 10), dtype=np.float)
     # no usable ace state
-    N_ACE_V = np.zeros((10, 10))
+    N_ACE_V = np.zeros((10, 10), dtype=np.float)
+    N_ACE_C = np.zeros((10, 10), dtype=np.float)
     # 200 states in total (no/usable ACE)
     # V[0]: No Usable Ace
     # V[1]: Usable Ace
     V = [N_ACE_V, U_ACE_V]
+    # counting
+    C = [N_ACE_C, U_ACE_C]
     
+    show_states = [None,
+              None,
+              None,
+              None]
+
+    titles = ['Usable Ace, 10000 Episodes',
+              'Usable Ace, 500000 Episodes',
+              'No Usable Ace, 10000 Episodes',
+              'No Usable Ace, 500000 Episodes']
+
+    eposide_c = 0
     while True:
         # state: (p_sum, d_show, u_ace: 0-no; 1-yes), p_sum can < 12 or bust
         # action: stick-0; hit-1
         # reward: -1, 0, 1
+        # random starting point
         states, actions, rewards = bj_eposide_gen(player_simple_policy, dealer_simple_policy)
-        # print(states, rewards)
-        # pdb.set_trace()
         
-    
+        # state & action correspond to t
+        # reward corresponds to t+1        
+
+        assert len(states) == len(actions) == len(rewards)
+        t_len = len(states)
+        
+        # G_T = 0
+        Return = 0.0
+
+        for t_step in reversed(range(t_len)):
+            state = states[t_step]
+            p_sum, dealer_show, ace_heat = state
+            reward = rewards[t_step]
+
+            # G_t = R_{t+1} + discount * G_{t+1}
+            Return = reward + DISCOUNT * Return
+
+            # One advantage of MC method !!!!!!
+            # update the meaningful states only
+            if 12 <= p_sum <= 21:
+                if state in states[:t_step]:
+                    # not first visit here
+                    pass
+
+                else:
+                    # first visit here
+                    # update the very first state only 
+                    assert ace_heat in [0, 1]
+                    avg_v = V[ace_heat][p_sum-12, dealer_show-1]
+                    count = C[ace_heat][p_sum-12, dealer_show-1]
+
+                    V[ace_heat][p_sum-12, dealer_show-1] = (avg_v * count + Return)/(count+1.0)
+                    C[ace_heat][p_sum-12, dealer_show-1] = count+1.0
+
+        eposide_c += 1    
+
+        if eposide_c == 10000:
+            print('{} eposide Done.'.format(eposide_c))
+            show_states[0] = np.copy(V[1])
+            show_states[2] = np.copy(V[0])
+            # pdb.set_trace()
+        elif eposide_c == 500000:
+            print('{} eposide Done.'.format(eposide_c))
+            show_states[1] = np.copy(V[1])
+            show_states[3] = np.copy(V[0])
+            # pdb.set_trace()
+            break
+
+    # drawing
+    _, axes = plt.subplots(2, 2, figsize=(40, 30))
+    plt.subplots_adjust(wspace=0.1, hspace=0.2)
+    axes = axes.flatten()
+
+    for show_state, title, axis in zip(show_states, titles, axes):
+        fig = sns.heatmap(np.flipud(show_state), cmap="YlGnBu", ax=axis, xticklabels=range(1, 11),
+                          yticklabels=list(reversed(range(12, 22))))
+        fig.set_ylabel('player sum', fontsize=30)
+        fig.set_xlabel('dealer showing', fontsize=30)
+        fig.set_title(title, fontsize=30)
+
+    plt.savefig('./images/mine/figure_5_1.png')
+    plt.close()
+
 
 if __name__ == '__main__':
     figure_5_1()
